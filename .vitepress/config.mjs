fe98d50a1e54d32b4a5da200c41bc6b9784c04bd
CaoKai <2655257336@qq.com>
2025-02-23 10:36:42 +0800

    'demo'

---
+++ .vitepress/config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Vue.Js设计与实现笔记",
+  description: "学习与思考",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "https://vuejs.org/images/logo.png",
+    outlineTitle: "目录",
+    outline: "deep",
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "博客", link: "https://www.caok.top/" },
+      {
+        text: "项目",
+        items: [
+          { text: "项目A", link: "https://www.caok.top/" },
+          { text: "项目B", link: "https://www.caok.top/" },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        text: "第一章",
+        items: [{ text: "权衡的艺术", link: "/Article/第一章/权衡的艺术.md" }],
+      },
+      {
+        text: "第二章",
+        items: [{ text: "Vue3.0", link: "/markdown-examples.md" }],
+      },
+      {
+        text: "第三章",
+        items: [{ text: "demo", link: "/api-examples.md" }],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Vue.Js设计与实现"
+  text: "记录与分享 Vue.js 核心知识"
+  tagline: 书读百遍，其义自见
+  image:
+    src: "https://vuejs.org/images/logo.png"
+  actions:
+    - theme: brand
+      text: 开始阅读
+      link: /markdown-examples
+    - theme: alt
+      text: 问一问AI
+      link: https://chat.deepseek.com/
+
+features:
+  - title: 📖每日5页
+    details: 逐字阅读书籍内容，整理核心知识点，深入理解 Vue.js 的设计思想和实现原理
+  - title: 🔎源码剖析
+    details: 结合 Vue.js 源码，解析关键实现细节，掌握核心架构，提升源码阅读能力
+  - title: ✒️实践与思考
+    details: 结合实际开发经验，分析 Vue.js 在项目中的应用场景，记录思考与最佳实践
+---
